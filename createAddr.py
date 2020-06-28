41ec1b02ca5f95f1417ae0fca69a3a44b5aadebb
djalillounis <djalillounis3@gmail.com>
2020-06-28 20:15:14 +0200

    Push

--- createAddr.py
+++ createAddr.py
@@ -9,31 +9,30 @@ from config import sendCmdtoShell,snedCmdtoSell_testing,configParms,Mkdir
 
 def createPayAddr (_name):
     wltDirbool = Mkdir(_name)
-    if wltDir:
-        wltDir=_name
+    if wltDirbool:
+        wltDir=_name+"/"
         parms =configParms()
         if len(parms) >= 1 :
-            name =_name+".key"
+            name =wltDir+_name+".key"
 
             signing_key_file = name+".skey"
             verification_key_file=name+".vkey"
             cmd ="cardano-cli shelley address key-gen "+" --verification-key-file "+verification_key_file +" --signing-key-file "+ signing_key_file
             sendCmdtoShell(cmd)
-            cmd =""
-            cmd ="mkdir ./name"
-            sendCmdtoShell(cmd)
-            name=""
 
 
-            addrFile =_name+".pyment.addr"
-            print(verification_key_file)
+
+
+
+            addrFile =wltDir+_name+".pyment.addr"
+
 
             cmd = parms[0] + " address build --payment-verification-key-file "+verification_key_file+" --out-file " + addrFile +" " +  parms[1]
             sendCmdtoShell(cmd)
             print("--------------------------------------------")
             print("Address created " +addrFile)
             Hexa = sendCmdtoShell("cat "+ addrFile)
-            addr = Hexa.decode('utf-8')
+            
             print(addr)
 
             print("--------------------------------------------")
